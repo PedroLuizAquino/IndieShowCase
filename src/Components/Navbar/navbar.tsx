9c95d5cef7892c957828734102b8e4e334f93ba7
pedro.luiz <pedro.aquino@liax.tech>
2023-12-06 20:00:21 -0300

    decodificando o token na navbar

--- src/Components/Navbar/navbar.tsx
+++ src/Components/Navbar/navbar.tsx
@@ -9,81 +9,70 @@ import { Drawer, TextField } from "@mui/material";
 import { MenuDrawer } from "../MenuDrawer";
 import { SearchBar } from "../SearchBar/SearchBar";
 import { useEffect, useState } from "react";
+import { jwtDecode } from "jwt-decode";
+
 
 export const Navbar = () => {
   const navigate = useNavigate();
   const [token, setToken] = useState<string | null>(null);
+  const [userName, setUserName] = useState<string | null>(null);
+  const [userID, setUserId] = useState<number | null>(null);
 
   useEffect(() => {
-    const userToken = localStorage.getItem("token"); // exemplo de onde você pode armazenar o token
+    const userToken = localStorage.getItem('token');
     setToken(userToken);
+
+    // Exemplo: obtendo informações do usuário ao fazer login
+    if (userToken) {
+      const tokenDecodificado = jwtDecode<{ usu_id: number, usu_nome: string }>(userToken);
+      const userID = tokenDecodificado?.usu_id;  // <-- Renomeie aqui
+      const userName = tokenDecodificado?.usu_nome;
+      // Suponha que você tenha um endpoint no seu backend para obter as informações do usuário
+      fetch(`http://localhost:8000/usuarios/${userID}`, {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${userToken}`
+        },
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log('Dados recebidos no frontend:', data);
+          const userData = data.response[0];
+          setUserName(userData.usu_nome);
+          setUserId(userData.usu_id);
+
+        })
+        .catch(error => console.error('Erro ao obter informações do usuário:', error));
+    }
   }, []);
 
-  console.log("Renderizou");
-
   return (
-    <Box marginBottom={"100px"}>
-      <AppBar position="fixed" color="primary">
+    <Box marginBottom={'100px'}>
+      <AppBar position="fixed" color='primary'>
         <Toolbar>
-          <Box paddingRight={2} display={"flex"} color={"inherit"}>
+          <Box paddingRight={2} display={'flex'} color={'inherit'}>
             <MenuDrawer />
           </Box>
-          <Typography
-            variant="h6"
-            component={Button}
-            onClick={() => navigate("/")}
-            color={"white"}
-          >
+          <Typography variant="h6" component={Button} onClick={() => navigate('/')} color={'white'}>
             IndieShowCase
           </Typography>
-          <Box display={"flex"} justifyContent={"center"} sx={{ flexGrow: 1 }}>
-            {/* <TextField
-                            type='search'
-                            sx={{ width: '400px' }}
-                            placeholder="Search..."
-                            variant="outlined"
-                            size='small'
-                            color='secondary'
-                        /> */}
+          <Box display={'flex'} justifyContent={'center'} sx={{ flexGrow: 1 }}>
             <SearchBar />
           </Box>
           {token ? (
-            <Box gap={1} display={"flex"}>
-              <Button
-                color="pedro"
-                variant="text"
-                onClick={() => navigate("/criarPostagem")}
-              >
-                Publicar
-              </Button>
-              <Button
-                variant="text"
-                color="pedro"
-                onClick={() => {
-                  localStorage.removeItem("token");
-                  navigate("/");
-                  window.location.reload();
-                }}
-              >
-                Logout
-              </Button>
+            <Box gap={1} display={'flex'}>
+              <Button color='pedro' variant='text' onClick={() => navigate(`/perfil/${userID}`)}> {userName}  </Button>
+              <Button color='pedro' variant='text' onClick={() => navigate('/criarPostagem')}>Publicar</Button>
+              <Button variant='text' color='pedro' onClick={() => {
+                localStorage.removeItem('token');
+                navigate('/');
+                window.location.reload();
+              }}>Logout</Button>
             </Box>
           ) : (
-            <Box gap={1} display={"flex"}>
-              <Button
-                color="pedro"
-                variant="text"
-                onClick={() => navigate("/cadastroUsuario")}
-              >
-                Cadastrar
-              </Button>
-              <Button
-                variant="text"
-                color="pedro"
-                onClick={() => navigate("/login")}
-              >
-                Login
-              </Button>
+            <Box gap={1} display={'flex'}>
+              <Button color='pedro' variant='text' onClick={() => navigate('/cadastroUsuario')}>Cadastrar</Button>
+              <Button variant='text' color='pedro' onClick={() => navigate('/login')}>Login</Button>
             </Box>
           )}
         </Toolbar>
